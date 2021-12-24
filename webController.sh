fa52bb1d36116ab111c0e88da113a1050d2d52e5
NeverHackM3 <96632840+NeverHackM3@users.noreply.github.com>
2021-12-24 20:10:44 +0100

    Update webController.sh

--- webController.sh
+++ webController.sh
@@ -6,7 +6,6 @@ for str in ${WEBS[@]}; do
   HTTP_STATUS="$(curl -I $str 2>&1 | awk '/HTTP\// {print $2}')";
     if  [[ $HTTP_STATUS != "200" ]];
      then
-          echo "NO activo, reiniciando";
           service apache2 restart;
           exit 1;
     fi
